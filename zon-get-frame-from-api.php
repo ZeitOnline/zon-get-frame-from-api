bd53313bb508af3451eb330ed33f2b26cde08ea3
Nico Bruenjes <nico.bruenjes@gmail.com>
2017-12-28 13:11:48 +0100

    Update silly script kiddie die and bump version

--- zon-get-frame-from-api.php
+++ zon-get-frame-from-api.php
@@ -5,7 +5,7 @@
  * Plugin Name:       ZEIT ONLINE Framebuilder Client
  * Plugin URI:        https://github.com/ZeitOnline/zon-get-frame-from-api
  * Description:       Get and cache a preconfigured site frame from www.zeit.de/framebuilder and display it as header and footer in the blog themes
- * Version:           2.0.1
+ * Version:           2.0.2
  * Author:            Nico Bruenjes, Moritz Stoltenburg, Arne Seemann
  * Author URI:        http://www.zeit.de
  * Text Domain:       zgffa
@@ -15,7 +15,7 @@
  * GitHub Plugin URI: https://github.com/ZeitOnline/zon-get-frame-from-api
 */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+! defined( 'ABSPATH' ) and exit;
 
 class ZON_Get_Frame_From_API
 {
